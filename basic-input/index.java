0a9c29cd57050bdea79788613d0e63f6849efb2c
intelliapps-io <jared.moore@intelliapps.io>
2019-02-01 16:11:20 -0500

    edits

---
+++ basic-input/index.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class index {
+  public static void main(String[] args) {
+    System.out.println("Enter a number!");
+
+    int input; // Declare var
+
+    Scanner keyboard = new Scanner(System.in); // Create instance of Scanner class
+    input = keyboard.nextInt(); // Get user input
+
+    keyboard.close();
+    
+    System.out.println("Your Number Is: " + input);
+  }
+}
